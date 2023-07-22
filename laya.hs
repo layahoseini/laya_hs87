7d3d9eb1accc7cddda3295f0de49624bc46d112a
layahoseini <139786530+layahoseini@users.noreply.github.com>
2023-07-23 01:40:12 +0330

    Create laya.hs

---
+++ laya.hs
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Green land</title>
+    <link rel="stylesheet" href="./assets/styles/main.css">
+</head>
+<body>
+    <header>
+        <div>
+        <img src="./assets/images/aks.png" width="1500" height="650" alt="shop photo">
+        </div>
+        <hr>
+        <a href="./assets/views/contact.html">contact with us</a>
+        <div class="parent">
+            <div>
+                <img src="./assets/images/schefflera-elegantissima.webp" width="100" height="100" alt="Schefflera Elegantissima"> 
+                <h3>Schefflera Elegantissima</h3>
+                <button>
+                    choose it
+                </button>
+            </div>
+            <div>
+                <img src="./assets/images/Adenium.webp" width="100" height="100" alt="Adenium">
+                <h3>Adenium</h3>
+                <button>
+                    choose it
+                </button>
+            </div>
+            <div>
+                <img src="./assets/images/fatsia-japonica.webp" width="100" height="100" alt="Fatsia Japonica">
+                <h3>Fatsia Japonica</h3>
+                <button>
+                    choose it
+                </button>
+            </div>
+            <div>
+                <img src="./assets/images/aeschynanthus-japhrolepsis.webp" width="100" height="100" alt="Aeschynanthus Japhrolepsis">
+                <h3>Aeschynanthus Japhrolepsis</h3>
+                <button>
+                    choose it
+                </button>
+            </div>
+            <div>
+                <img src="./assets/images/African-daisy.webp" width="100" height="100" alt="African Daisy">
+                <h3>African Daisy</h3>
+                <button>
+                    choose it
+                </button>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
